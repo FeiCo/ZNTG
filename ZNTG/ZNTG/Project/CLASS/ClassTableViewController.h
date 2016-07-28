fd5f054d900c3690217eff0116494a56ebe4e481
HeterPu <wycgpeterhu@sina.com>
2016-07-28 10:49:29 +0800

    增加PCH哦

---
+++ ZNTG/ZNTG/Project/CLASS/ClassTableViewController.h
@@ -0,0 +1,18 @@
+//
+//  ClassTableViewController.h
+//  ZNTG客户端
+//
+//  Created by Peter on 5/30/16.
+//  Copyright © 2016 Peter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+
+
+@interface ClassTableViewController : UITableViewController
+
+
+
+@end
