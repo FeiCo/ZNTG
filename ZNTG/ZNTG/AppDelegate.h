307807126c26729ae16163c25b5294be08f538fd
HeterPu <wycgpeterhu@sina.com>
2016-07-28 10:29:54 +0800

    增加了目录

--- ZNTG/ZNTG/AppDelegate.h
+++ ZNTG/ZNTG/AppDelegate.h
@@ -2,8 +2,8 @@
 //  AppDelegate.h
 //  ZNTG
 //
-//  Created by Peter on 7/27/16.
-//  Copyright © 2016 Peter. All rights reserved.
+//  Created by Peter Hu on 16/7/27.
+//  Copyright © 2016年 Peter Hu. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
--- ZNTG/ZNTG/AppDelegate.m
+++ ZNTG/ZNTG/AppDelegate.m
@@ -2,11 +2,18 @@
 //  AppDelegate.m
 //  ZNTG
 //
-//  Created by Peter on 7/27/16.
-//  Copyright © 2016 Peter. All rights reserved.
+//  Created by Peter Hu on 16/7/27.
+//  Copyright © 2016年 Peter Hu. All rights reserved.
 //
 
 #import "AppDelegate.h"
+#import "TeacherViewController.h"
+#import "GeRenTableViewController.h"
+#import "HomeViewController.h"
+#import "NaviViewController.h"
+#import "MainTabViewController.h"
+
+#import "ClassTableViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,30 +23,47 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    
+//配置TabbarController
+    HomeViewController *homeC = [[HomeViewController alloc] init];
+    TeacherViewController *teacherC = [[TeacherViewController alloc] init];
+    ClassTableViewController *classC = [[ClassTableViewController alloc] init];
+    GeRenTableViewController *meC = [[GeRenTableViewController alloc] init];
+    
+    NaviViewController *navi1 = [[NaviViewController alloc] initWithRootViewController:homeC];
+    NaviViewController *navi2 = [[NaviViewController alloc] initWithRootViewController:teacherC];
+    NaviViewController *navi3 = [[NaviViewController alloc] initWithRootViewController:classC];
+    NaviViewController *navi4 = [[NaviViewController alloc] initWithRootViewController:meC];
+    
+    NSArray *controllerarray = [[NSArray alloc] initWithObjects:navi1,navi2,navi3,navi4, nil];
+    MainTabViewController *tabC = [[MainTabViewController alloc] init];
+    tabC.viewControllers = controllerarray;
+    tabC.selectedIndex = 0;
+    self.window.rootViewController = tabC;
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+   
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    
 }
 
 @end
